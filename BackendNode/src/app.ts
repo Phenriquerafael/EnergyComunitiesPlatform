20332ab414cd77229447d635e3c39e29c62c0f10
Phenriquerafael <mailpedrohenrique@gmail.com>
2025-04-29 17:46:30 +0100

    #9 Cors setup added

--- BackendNode/src/app.ts
+++ BackendNode/src/app.ts
@@ -9,6 +9,16 @@ import Logger from './loaders/logger';
 async function startServer() {
   const app = express();
 
+  const cors = require('cors');
+
+  const corsOptions = {
+    origin: ['http://localhost:5173','http://localhost:8000'], 
+    methods: ['GET', 'POST', 'PUT', 'DELETE','PATCH'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  };
+
+  app.use(cors(corsOptions)); // Use o middleware CORS com as opções definidas
+
   await require('./loaders').default({ expressApp: app });
 
   app.listen(config.port, () => {
